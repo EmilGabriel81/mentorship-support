fe206c5e8051d7e8fb6ebd23d6eeefd826cc6422
EmilGabriel81 <emilgabriel@gmail.com>
2023-05-01 23:09:14 +0300

    Added the ShowUser activity

--- app/src/main/java/com/example/mentorshipsupport01/DisplayUsersActivity.java
+++ app/src/main/java/com/example/mentorshipsupport01/DisplayUsersActivity.java
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 import android.util.Log;
 
-import android.util.Log;
 import com.example.mentorshipsupport01.recyclerview.UserAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -61,7 +61,10 @@ public class DisplayUsersActivity extends AppCompatActivity {
                 RecyclerView recyclerView = findViewById(R.id.recyclerview);
                 recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                 UserAdapter adapter = new UserAdapter(getApplicationContext(), users);
-                adapter.setOnItemClickListener(user -> Toast.makeText(DisplayUsersActivity.this, "Item was clicked: " + user.getFirstName() + " " + user.getLastName(), Toast.LENGTH_SHORT).show());
+                adapter.setOnItemClickListener(user -> {
+                    Toast.makeText(DisplayUsersActivity.this, "Item was clicked: " + user.getFirstName() + " " + user.getLastName(), Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(DisplayUsersActivity.this, ShowUserActivity.class));
+                });
                 recyclerView.setAdapter(adapter);
             }
 
